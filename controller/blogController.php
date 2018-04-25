caf7561b8b94ce7c7a814bcc789440366669ec5b
crambled <crambled@mhcc.edu>
2018-04-24 20:03:56 -0700

    Add blog module

---
+++ controller/blogController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+# Include html header
+include( APP_VIEW . '/header.php' );
+
+# Include main navigation
+include( APP_VIEW . '/nav.php' );
+
+switch ( $route->getAction() ) {
+
+    case 'create':
+      include( APP_VIEW .'/blog/listSubNav.php' );
+      include( APP_VIEW .'/blog/createView.php' );
+      break;
+
+    case 'list':
+        include( APP_VIEW .'/blog/listSubNav.php' );
+        include( APP_VIEW .'/blog/listView.php' );
+        break;
+
+    default:
+        include( APP_VIEW .'/blog/listSubNav.php' );
+        include( APP_VIEW .'/blog/listView.php' );
+        break;
+}
+
+
+# Include html footer
+include( APP_VIEW . '/footer.php' );
